bc6b17b26e6a083adb180df6cdfc13f8ad360035
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-03-25 22:09:23 +0100

    Ejercicio4 Main

--- Main.java
+++ Main.java
@@ -2,9 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         Cuadrado cuadrado1 = new Cuadrado();
+        Rectangulo rectangulo1 = new Rectangulo();
 
         Forma[] arrayMain = new Forma[3];
         arrayMain[0] = cuadrado1;
+        arrayMain[1] = rectangulo1;
+
         arrayMain[0].rellenar_forma();
+        arrayMain[1].dibujar_puntos();
     }
 }
