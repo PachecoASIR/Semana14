6e071486e70550d7f98824ecc817cfc9f621d8c6
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-03-25 22:11:11 +0100

    Ejercicio5

---
+++ Trigono.java
@@ -0,0 +1,28 @@
+public abstract class Trigono {
+
+    private Punto[] vertices = new Punto[3];
+
+    Trigono() {
+    }
+
+    /*
+    public int[] getVertices() {
+        //No podemos convertir Punto a int
+        return this.vertices;
+    }
+    */
+
+    public Punto[] getVertices() {
+        return this.vertices;
+    }
+
+    public void setPuntos(Punto[] vertices) {
+        this.vertices = vertices;
+    }
+
+    public void setVertices(Punto p1, Punto p2, Punto p3, Punto p4) {
+        vertices[0] = p1;
+        vertices[1] = p2;
+        vertices[2] = p3;
+    }
+}
